c29941c99bc3a2dad8238428ffce79c0f1f437c7
rickyushinahua-rgb <ricky.ushinahua@tecsup.edu.pe>
2026-06-03 17:34:29 -0500

    Corregido STATIC_ROOT para el entorno de Render

--- backend/settings.py
+++ backend/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    
+if os.environ.get('RENDER'):
+
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

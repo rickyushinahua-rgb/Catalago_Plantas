4d49877b75eb2c23131e35f9d5554c282e79aeac
Ricky Marty <ricky.ushinahua@tecsup.edu.pe>
2026-06-05 01:40:25 -0500

    Corregir configuracion staticfiles y Cloudinary

--- backend/settings.py
+++ backend/settings.py
@@ -165,6 +165,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # ==========================
 # CLOUDINARY PARA IMÁGENES
